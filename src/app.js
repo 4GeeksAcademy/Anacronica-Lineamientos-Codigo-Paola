f5f455cb36f41c46d84093d90422e47d8871dcbd
Paola Chávez <56763235+Anacronica@users.noreply.github.com>
2024-04-29 19:49:31 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,31 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomWhoIndex = Math.floor(Math.random() * who.length);
+  let randomActionIndex = Math.floor(Math.random() * action.length);
+  let randomWhatIndex = Math.floor(Math.random() * what.length);
+  let randomWhenIndex = Math.floor(Math.random() * when.length);
+
+  let excuse = `${who[randomWhoIndex]} ${action[randomActionIndex]} ${what[randomWhatIndex]} ${when[randomWhenIndex]}.`;
+
+  return excuse;
+}
+
+// Generar una excusa al cargar la página
+window.onload = function() {
+  let randomExcuse = generateExcuse();
+  document.getElementById("excuse").innerHTML = randomExcuse;
+};
